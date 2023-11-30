2a3631d9b020f344598f76c59ad246b52b582257
daianechen <143471226+daianechen@users.noreply.github.com>
2023-11-30 21:23:44 +0800

    project b draft

--- FINAL/sketch.js
+++ FINAL/sketch.js
@@ -2,6 +2,10 @@ let films = [];
 let film1;
 let filmRoll;
 
+let waterSound
+let beerSound 
+let mountainSound
+
 let startX = 0;
 let startY = -50;
 
@@ -22,7 +26,7 @@ function preload() {
     } 
     else if (i == 2) {
        // load sound
-      let waterSound = loadSound("library/water-waves.mp3");
+      waterSound = loadSound("library/water-waves.mp3");
       // load image
       let filename = "library/film" + i + ".jpg";
       let waterImg = loadImage(filename);
@@ -30,7 +34,7 @@ function preload() {
       films.push(the_img);
     } else if (i == 5) {
        // load sound
-      let beerSound = loadSound("library/beer-clink.mp3");
+      beerSound = loadSound("library/beer-clink.mp3");
       // load image
       let filename = "library/film" + i + ".jpg";
       let beerImg = loadImage(filename);
@@ -38,7 +42,7 @@ function preload() {
       films.push(the_img);
     } else if (i == 7) {
        // load sound
-      let mountainSound = loadSound("library/birds-singing.mp3");
+      mountainSound = loadSound("library/birds-singing.mp3");
       // load image
       let filename = "library/film" + i + ".jpg";
       let mountainImg = loadImage(filename);
@@ -91,19 +95,23 @@ class FilmRoll {
 
     // this.speed= -3; //loop later
     //this.speed = speed
+    this.soundPlay = false;
   }
   display() {
     push();
     translate(this.x, this.y);
     strokeWeight(2);
     fill(0);
+    let filmX = -10;
+    let filmY = windowHeight/7 -20;
     // background film roll
-    rect(-10, windowHeight/7 -20, 500* this.images.length, 640);
+    rect(filmX, filmY, 500* this.images.length, windowHeight*8/9);
+    //windowHeight*6/7
     
     fill(255);
     for (let i = 0; i < this.images.length; i++) {
-      rect(5 + i * 500, windowHeight / 5.5, 440, 550);
-      films[i].display(5 + i * 500, windowHeight / 5.5, 440, 550);
+      rect(5 + i * windowHeight*25/36, windowHeight / 5.5, (windowHeight*9/11-45)/5*4, windowHeight*9/11-45);
+      films[i].display(5 + i * windowHeight*25/36, windowHeight / 5.5, (windowHeight*9/11-45)/5*4, windowHeight*9/11-45); //550
       //call special images here
       // //if i==2, then films[i].mousepress
       // if (i == 2 || i == 5 || i == 7 ) {
@@ -113,8 +121,10 @@ class FilmRoll {
 
     //top & bottom squares
     for (let i = 0; i < 67; i++) {
-      rect(i * 60, windowHeight / 6.5 - 10, 15, 15);
-      rect(i * 60, windowHeight- 35, 15, 15);
+      // rect(i * 60, 105, 15, 15);
+      // rect(i * 60, 700, 15, 15);
+      rect(i * 60, ((filmY +20)*7)/6.5 - 10, 15, 15);
+      rect(i * 60, ((filmY +20)*7)- 35, 15, 15);
     }
 
 
@@ -138,7 +148,7 @@ class FilmRoll {
       }
     }
 
-    if (counter >= 300) {
+    if (counter >= 450) {
       clickCamera = false;
       counter = 0;
     }
@@ -150,13 +160,13 @@ class FilmRoll {
 
     //left button
     fill(this.rl, this.gl, this.bl);
-    ellipse(width / 4 + 60, height - 40 / 2 - 20, 40, 40);
+    ellipse(width / 4 + 60, height - 30 / 2 - 20, 40, 40);
     //fill("red");
     //text("Left", width / 4 + 60, height - 40 / 2 - 20);
 
     //right button
     fill(this.rr, this.gr, this.br);
-    ellipse((3 * width) / 4 - 60, height - 40 / 2 - 20, 40, 40);
+    ellipse((3 * width) / 4 - 60, height - 30 / 2 - 20, 40, 40);
     //fill("red");
     //text("Right", (3 * width) / 4 - 60, height - 40 / 2 - 20);
     pop();
@@ -213,33 +223,10 @@ class FilmRoll {
   }
   checkMouseEveryFrame() {
     this.buttonLogic();
+    console.log();
     // mouseover
     fill("red")
     // mouse in relation to window
-    let mX = mouseX;
-    let mY = mouseY
-    text(mouseX + " , " + mouseY, mouseX, mouseY );
-    // mouse in relation to window
-    let mXonFilm = mouseX - this.x
-    let mYonFIlm = mouseY;
-    text(mXonFilm + " , " + mYonFIlm, mouseX, mouseY+30 );
-
-    //if mouse hovered over then sound plays
-    //use if statements here for the 2 that play sounds (use the bottom number)
-  //   for (let i = 1; i < 9; i++) {
-  //     if (i == 2 ) {
-  //       water-waves.play();
-  //     } 
-  //     else if (i == 7) {
-  //       birds-singing.play();
-  // }
-}
-
-  checkMouseOnPress() {
-    this.buttonLogic();
-    // mouseover
-    fill("red")
-    // mouse in relation to window/browser
     let mX = mouseX;
     let mY = mouseY
     text(mouseX + " , " + mouseY, mouseX, mouseY );
@@ -248,11 +235,52 @@ class FilmRoll {
     let mYonFIlm = mouseY;
     text(mXonFilm + " , " + mYonFIlm, mouseX, mouseY+30 );
 
-    //use if statements here ()
-    // for (let i = 1; i < 9; i++) {
-    //   if (i == 5 ) {
-    //     beerclink.play();
-    //   } 
+    //if mouse hovered over then sound plays
+    //use if statements here for the 2 that play sounds (use the bottom number)
+    // if mouseIsPressed
+
+      if (mXonFilm > 505 && mXonFilm< 945 && mYonFIlm > 84 && mYonFIlm<632) {
+        // this.soundPlay = true
+        waterSound.play();
+
+      } else {
+        waterSound.stop();
+        this.soundPlay = false;
+      }
+
+      // if (mXonFilm > 505 && mXonFilm< 945 && mYonFIlm > 84 && mYonFIlm<632) {
+      //   this.soundPlay = true;
+      //   if (this.soundPlay == false) {
+      //     waterSound.play();
+      //   }
+      // }
+      
+      if (mXonFilm > 3005 && mXonFilm< 3445 && mYonFIlm > 84 && mYonFIlm<632){
+        mountainSound.play();
+      } else{
+        mountainSound.stop();
+      }
+    }
+  
+  checkMouseOnPress() {
+    this.buttonLogic();
+    // mouseover
+    // fill("red")
+    // // mouse in relation to window/browser
+    // let mX = mouseX;
+    // let mY = mouseY
+    // text(mouseX + " , " + mouseY, mouseX, mouseY );
+    // // mouse in relation to film
+    // let mXonFilm = mouseX - this.x
+    // let mYonFIlm = mouseY;
+    // text(mXonFilm + " , " + mYonFIlm, mouseX, mouseY+30 );
+
+    if (mXonFilm > 2217 && mXonFilm< 2280 && mYonFIlm > 352 && mYonFIlm<490){
+      beerSound.play();
+    } else {
+      beerSound.stop();
+      this.soundPlay = false;
+    }
 
   }
 
@@ -262,6 +290,7 @@ class FilmRoll {
     rect(windowWidth - 50, 0, 100, 700, 55, 55); // side lens
     rect(windowWidth - 30, -10, 30, 40, 40, 40);
   }
+
 }
 
 function mousePressed() {
